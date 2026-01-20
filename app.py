731a5dcb1407c1289c156825aad5be0bb3dac20d
Nahue18R <nahuelruizz18@gmail.com>
2026-01-20 12:27:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ st.set_page_config(page_title="Sistema RRHH - Open25", layout="wide", initial_si
 # ⚠️ TUS LINKS DE NGROK
 # ==========================================
 BASE_URL = "https://spring-hedgeless-eccentrically.ngrok-free.dev" # <--- ¡VERIFICA QUE ESTE SEA EL TUYO!
-WEBHOOK_SOLICITUD = f"http://localhost:5678/webhook/solicitud-vacaciones"
-WEBHOOK_APROBACION = f"http://localhost:5678/webhook/solicitud-aprobada"
+WEBHOOK_SOLICITUD = f"{BASE_URL}/webhook/solicitud-vacaciones"
+WEBHOOK_APROBACION = f"{BASE_URL}/webhook/solicitud-aprobada"
 
 # --- ESTILOS CSS ---
 st.markdown("""
@@ -320,4 +320,5 @@ elif menu == "📅 Calendario":
                     "color": c
                 })
     calendar(events=ev, options={"initialView": "dayGridMonth", "locale": "es"})
+
 
